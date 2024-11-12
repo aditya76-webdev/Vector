7dc496242d6606ffd9cff32e10df001df20f85c1
Aditya Jain <adityajain77356@gmail.com>
2024-11-12 17:04:16 +0530

    vectors for endsem

---
+++ College/vectorCreation.java
@@ -0,0 +1,18 @@
+package College;
+
+import java.util.Vector;
+
+public class vectorCreation {
+    public static void main(String[] args) {
+        Vector v1 = new Vector();
+        v1.add(1);
+        v1.add(2);
+        v1.add(3);
+        v1.add("java");
+        v1.add(true);
+        v1.add("program");
+        v1.insertElementAt(10,0);
+//        v1.clear();
+        System.out.println(v1);
+    }
+}
